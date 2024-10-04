53dc1abf7daaffec2426e4b2a47538a8d26cd8bf
mohamed <harchim94@gmail.com>
2024-10-04 17:22:18 +0100

    fix the error

--- elharchiPortfolio/src/App.jsx
+++ elharchiPortfolio/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Themes from "./components/Themes";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Contact from "./pages/contact/Contact";
